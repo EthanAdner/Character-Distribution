f4367d517da77870ca8c6f52bf7b38e6ec4e9102
EthanAdner <ethanda99@gmail.com>
2016-02-26 14:45:41 -0500

    Updated from Brython Server: 2/26/2016 2:45:42 PM

--- distribution.py
+++ distribution.py
@@ -40,4 +40,4 @@ s=str(input("Please enter a string of text (the bigger the better):"))
 
 l=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 def count_letters(s):
-    #for x in l:
+    s=s.lower()
